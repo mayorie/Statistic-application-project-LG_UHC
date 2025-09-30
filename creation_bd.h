3ef7c225330fad0d51092b32dffcdd5e6ada5445
mayorie <mattsachamahe@gmail.com>
2025-09-30 10:44:44 +0200

    ajout sur le sqlite
    ajout des fonction :
    create_stats_lguhc
    insert_game
    select_all_from_table

--- creation_bd.h
+++ creation_bd.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "sqlite3.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 int create_stats_lguhc();
+int insert_game(int id_role, int camp, std::string start_game, std::string event_ingame, bool win, std::string comment);
+int select_all_from_table(const std::string& tableName);
--- main.cpp
+++ main.cpp
@@ -15,6 +15,19 @@ int main() {
 	// création base de donnée.
 	create_stats_lguhc();
 
+	//insert une game
+	insert_game(
+		1,                                  // id_role
+		1,                                  // camp
+		"2025-10-05 21:00:00",              // start_game
+		"Le village a découvert un loup",   // event_ingame
+		1,
+		"Victoire du village !"             // comment
+	);
+
+	//select all sur une table
+	select_all_from_table("game"); //"game", "role", "role_camp", "gameplay"
+
 	//création du fichier des log bruts et récupération de ces données
 	log_brut fichier_log(chemin);
 
